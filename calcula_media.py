b0de7db6f76e38ddd22b632d756980d8b558ce13
Yagofs <yagofsarmento8@gmail.com>
2025-09-01 20:22:55 -0300

    Solução Finalizada

--- calcula_media.py
+++ calcula_media.py
@@ -1,1 +1,12 @@
 # Sua solução aqui
+"""CÓDIGO PARA CALCULAR A MÉDIA DE 5 NÚMEROS"""
+#Inserção de valores
+a = float(input())
+b = float(input())
+c = float(input())
+d = float(input())
+e = float(input())
+#Cálculo da média
+media = (a + b + c + d + e) / 5
+#Saída do resultado
+print(f"{media:.2f}")
